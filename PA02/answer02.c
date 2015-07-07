33dbdb8edaa78dacc31e09e080c00bacd16ac6e5
curtuscd <duffc@purdue.edu>
2015-07-07 13:58:46 -0400

    did the first one

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -1,10 +1,27 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "answer03.h"
 
 char * strcat_ex(char * * dest, int * n, const char * src)
 {
-
+size_t src_len = strlen(src);
+size_t len_dest = *dest==NULL ? 0 : strlen(*dest); 
+  if(*dest == NULL || len_src + len_dest + 1 >= *n)
+    {
+      *n=1 + (2 * (len_dest + len_src));
+      char *buffer = malloc(*n * sizeof(char));
+      *buffer='\0';
+      if(*dest!=NULL)
+	{
+	  strcpy(buffer,*dest);
+	}
+      free(*dest);
+      *dest = buffer;
+    }
+  strcat(*dest,src);
+  return *dest;
+}
 
 
 }
